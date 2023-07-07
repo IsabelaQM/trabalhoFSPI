ddda60e1dfc743d5ad22d9e8fe70f7b94ecedadf
Isabela Queiroz <106333972+IsabelaQM@users.noreply.github.com>
2023-07-07 16:23:41 -0300

    Update script.js

--- codigosFSPI/script/script.js
+++ codigosFSPI/script/script.js
@@ -1,107 +1,59 @@
+function convertFromDecimal(fromId, toBinaryId, toHexadecimalId) {
+  var fromInput = document.getElementById(fromId);
+  var toBinaryInput = document.getElementById(toBinaryId);
+  var toHexadecimalInput = document.getElementById(toHexadecimalId);
 
-function convert(from) {
-    var binaryInput = document.getElementById('binary');
-    var decimalInput = document.getElementById('decimal');
-    var hexadecimalInput = document.getElementById('hexadecimal');
+  var decimal = parseInt(fromInput.value);
 
-    var binaryValue = binaryInput.value.trim();
-    var decimalValue = decimalInput.value.trim();
-    var hexadecimalValue = hexadecimalInput.value.trim();
+  // Converter para binário
+  var binary = decimal.toString(2);
+  toBinaryInput.value = binary;
 
-    if (from === 'binary') {
-        if (binaryValue !== '') {
-            decimalValue = parseInt(binaryValue, 2).toString(10);
-            hexadecimalValue = parseInt(binaryValue, 2).toString(16).toUpperCase();
-        } else {
-            decimalValue = '';
-            hexadecimalValue = '';
-        }
-    } else if (from === 'decimal') {
-        if (decimalValue !== '') {
-            binaryValue = parseInt(decimalValue, 10).toString(2);
-            hexadecimalValue = parseInt(decimalValue, 10).toString(16).toUpperCase();
-        } else {
-            binaryValue = '';
-            hexadecimalValue = '';
-        }
-    } else if (from === 'hexadecimal') {
-        if (hexadecimalValue !== '') {
-            binaryValue = parseInt(hexadecimalValue, 16).toString(2);
-            decimalValue = parseInt(hexadecimalValue, 16).toString(10);
-        } else {
-            binaryValue = '';
-            decimalValue = '';
-        }
-    }
-
-    binaryInput.value = binaryValue;
-    decimalInput.value = decimalValue;
-    hexadecimalInput.value = hexadecimalValue;
-
-    document.getElementById('binaryResult').textContent = 'Binário: ' + binaryValue;
-    document.getElementById('decimalResult').textContent = 'Decimal: ' + decimalValue;
-    document.getElementById('hexadecimalResult').textContent = 'Hexadecimal: ' + hexadecimalValue;
+  // Converter para hexadecimal
+  var hexadecimal = decimal.toString(16);
+  toHexadecimalInput.value = hexadecimal.toUpperCase();
 }
 
-function copyResult() {
-    var result = "";
-    var binaryResult = document.getElementById('binaryResult').textContent;
-    var decimalResult = document.getElementById('decimalResult').textContent;
-    var hexadecimalResult = document.getElementById('hexadecimalResult').textContent;
+// Função para converter de binário para decimal e hexadecimal
+function convertFromBinary(fromId, toDecimalId, toHexadecimalId) {
+  var fromInput = document.getElementById(fromId);
+  var toDecimalInput = document.getElementById(toDecimalId);
+  var toHexadecimalInput = document.getElementById(toHexadecimalId);
 
-    result += binaryResult + "\n";
-    result += decimalResult + "\n";
-    result += hexadecimalResult;
+  var binary = fromInput.value;
 
-    navigator.clipboard.writeText(result)
-        .then(function () {
-            alert('Resultado copiado para a área de transferência!');
-        })
-        .catch(function (error) {
-            console.error('Erro ao copiar o resultado: ', error);
-        });
+  // Converter para decimal
+  var decimal = parseInt(binary, 2);
+  toDecimalInput.value = decimal;
+
+  // Converter para hexadecimal
+  var hexadecimal = decimal.toString(16);
+  toHexadecimalInput.value = hexadecimal.toUpperCase();
 }
 
-function converterBinDec() {
-    var binario = document.getElementById("binario").value;
-    var decimal = binarioParaDecimal(binario);
-    document.getElementById("resultadoDecimal").textContent = "Decimal: " + decimal;
-}
-function converterDecBin() {
-    var decimal = document.getElementById("decimal").value;
-    var binario = decimalParaBinario(decimal);
-    document.getElementById("resultadoBinario").textContent = "Dinário: " + binario;
-}
-function converterDecHex() {
-    var decimal = document.getElementById("decimalHex").value;
-    var hex = decimalParaHexadecimal(decimal);
-    document.getElementById("resultadoHexadecimal").textContent = "Hexadecimal: " + hex;
+// Função para converter de hexadecimal para decimal e binário
+function convertFromHexadecimal(fromId, toDecimalId, toBinaryId) {
+  var fromInput = document.getElementById(fromId);
+  var toDecimalInput = document.getElementById(toDecimalId);
+  var toBinaryInput = document.getElementById(toBinaryId);
+
+  var hexadecimal = fromInput.value;
+
+  // Converter para decimal
+  var decimal = parseInt(hexadecimal, 16);
+  toDecimalInput.value = decimal;
+
+  // Converter para binário
+  var binary = decimal.toString(2);
+  toBinaryInput.value = binary;
 }
 
-function converterHexDec() {
-    var hex = document.getElementById("hexadecimalDec").value;
-    var decimal = hexadecimalParaDecimal(hex);
-    document.getElementById("resultadoDecimalHex").textContent = "Decimal: " + decimal;
-}
+// Função para copiar o valor para a área de transferência e exibir um alerta
+function copyToClipboard(elementId) {
+  var element = document.getElementById(elementId);
+  element.select();
+  element.setSelectionRange(0, 99999); // Para dispositivos móveis
 
-function decimalParaBinario(decimal) {
-    return parseInt(decimal).toString(2);
-}
-function binarioParaDecimal(binario) {
-    var decimal = 0;
-    var posicao = 0;
-    while (binario > 0) {
-        var digito = binario % 10;
-        decimal += digito * Math.pow(2, posicao);
-        binario = Math.floor(binario / 10);
-        posicao++;
-
-    }
-    return decimal;
-}
-function decimalParaHexadecimal(decimal) {
-    return parseInt(decimal).toString(16).toUpperCase();
-}
-function hexadecimalParaDecimal(hexadecimal) {
-    return parseInt(hexadecimal, 16);
+  document.execCommand("copy");
+  alert("Número copiado para a área de transferência!");
 }
